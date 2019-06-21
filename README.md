6362a54dd5896c477181e30579bb53af5649d180
Mark Ghoprial <markghoprial@Marks-MacBook-Pro.local>
2019-06-20 20:57:14 -0700

    corrected timer.  added visible timer. still need to work out kinks with options

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # Triviagame
-//*****Psu-Psu-Psuco-Code-Psu-Psu-Psuco-Code-**************************
+
+
+This is the hardest quiz ever created.  You have been warned...
+
+//*****Psu-Psu-Psudo-Code-Psu-Psu-Psudo-Code-**************
 //start button
 //make a timer that counts down from 30 seconds.  Alert if Time is up
 //create multiple choice or T/F questions with checkmarks that fill
 //need to create a "complete" button
 // tally a score when "complete" button is hit while also stopping timer
-//********************************************************************
+//*********************************************************
+still need to make buttons pick true OR false
+the alert should show the score of the user
+complete button should finish quiz
+
+
+need to store answers on what people click
+
+
+make sure T/F are showing
+something is wrong with their values 
+
+compare the values and update a score
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,103 +1,161 @@
-let gameTimer;
 
-const questions = 
-[
-    { prompt: "You exist, correct?",
-    answer: true
-    },
-    { prompt: "Ringo Starr is the best Beatle",
-    answer: false
-    },
-    { prompt: "True or flase?",
-    answer: true 
-    },
-    { prompt: "Lizard people will control the world one day",
-    answer: true
-    },
-    { prompt: "Boop-bop boop-boop is waaaay better than bop-bop-boop",
-    answer: true
-    },
-    { prompt: "Mark finally started his hw at a reasonable time and will turn it in with zero stress or worry",
-    answer: false
-    }
-]
-var score = 0;
+$(document).ready(function () {
+
+    var gameTimer = 30;
+    console.log("timer" + gameTimer);
 
 
-function renderQuesions(){
-console.log("rendering questions" + renderQuesions);
-
-let $ol= $("<ol>");
-
-for (let i=0; i<questions.length; i++){
-    
-    let $li = $('<li>');
-    $li.text(questions[i].prompt);
-
-    let $trueButton = $("<button>");
-    $trueButton.text('true');
-    $trueButton.attr('data-answer', questions[i].prompt);
-    $trueButton.attr('class', "Btn");
-    $trueButton.attr('value', "true");
-    $trueButton.attr('id', "trueBtn");
-    $trueButton.attr('data-value', 'true')
-    $li.append($trueButton);
-    
-    
-    
-
-    
-    
-    let $falseButton = $("<button>");
-    $falseButton.text('false');
-    $falseButton.attr('data-answer', questions[i].prompt);
-    $falseButton.attr('class', "Btn");
-    $trueButton.attr('value', "false");
-    $falseButton.attr('id', "falseBtn");
-        $falseButton.attr('data-value', 'false');
-        $li.append($falseButton);
-        
-        $ol.append($li);
-    }
-    
-    
-    
-    $('.questions').append($ol);
-    
-}
- 
+    let intervalId;
 
 
 
-function stopGame() {
-    clearTimeout(gameTimer);
-    alert ("Quiz Over!")
-    //Your Score:0"/7");
-}
-
-
-
-
-$(document).ready(function() {
-    
-    $('#start-btn').on('click', function() {
-        $(this).hide();
-        alert ("you have 30 seconds to complete this very difficult quiz!")
-        renderQuesions();
-        gameTimer = setTimeout(stopGame, 30000);
-        $(".Btn").on('click', function(){
-            if(!$(this).val()){
-                $(this).addClass("btn btn-danger");
-                $("#trueBtn").attr("disabled", true);
-                
-            } else {
-                $(this).addClass("btn btn-primary");
-                $("#falseBtn").attr("disabled", true);
-
+    const questions =
+        [
+            {
+                prompt: "You exist, correct?",
+                answer: true
+            },
+            {
+                prompt: "Ringo Starr is the best Beatle",
+                answer: false
+            },
+            {
+                prompt: "True or flase?",
+                answer: true
+            },
+            {
+                prompt: "Lizard people will control the world one day",
+                answer: true
+            },
+            {
+                prompt: "Boop-bop boop-boop is waaaay better than bop-bop-boop",
+                answer: true
+            },
+            {
+                prompt: "Mark finally started his hw at a reasonable time and will turn it in with zero stress or worry",
+                answer: false
             }
-            console.log("clicked");
-        })
-    });
+        ]
+    var score = 0;
+    var game = $('.game') //this is a class because of                          the dot
 
-          
+
+    function renderQuesions() {
+
+        let $timer = $('<div>'); //created div tag in                               memory
+        $timer.attr('id', "timer"); //added id to div
+        game.append($timer); //added div to dom
+
+        console.log("timer" + gameTimer);
+
+        intervalId = setInterval(decrement, 1000);//this is what's decreasing the time by a second 
+        
+
+
+
+
+
+
+
+        let $ol = $("<ol>");
+
+        for (let i = 0; i < questions.length; i++) {
+
+            let $li = $('<li>');
+            $li.text(questions[i].prompt);
+
+            let $trueButton = $("<button>");
+            $trueButton.text('true');
+            $trueButton.attr('data-answer', questions[i].prompt);
+            $trueButton.attr('class', "Btn");
+            $trueButton.attr('value', "true");
+            $trueButton.attr('id', "trueBtn");
+            $trueButton.attr('data-value', 'true')
+            $li.append($trueButton);
+
+
+
+
+
+
+            let $falseButton = $("<button>");
+            $falseButton.text('false');
+            $falseButton.attr('data-answer', questions[i].prompt);
+            $falseButton.attr('class', "Btn");
+            $trueButton.attr('value', "false");
+            $falseButton.attr('id', "falseBtn");
+            $falseButton.attr('data-value', 'false');
+            $li.append($falseButton);
+
+            $ol.append($li);
+        }
+
+        let $completebutton = $("<button>");
+        $completebutton.text('complete');
+        $completebutton.attr('class, "Btn');
+        $completebutton.attr('id', "completeBtn");
+        $ol.append($completebutton);
+
+
+
+        $('.questions').append($ol);
+
+    }
+
+
+
+
+    function stopGame() {
+        clearInterval(intervalId);
+        alert("Quiz Over!")
+        //Your Score:0"/6");
+    }
+
+
+    $('#start-btn').on('click', function () {
+        $(this).hide();
+        alert("you have 30 seconds to complete this very difficult quiz!")
+        renderQuesions();
+
+    });
+    $(document).on('click', ".Btn", function () {
+        console.log($(this).val());
+        if (!$(this).val()) {
+            $(this).addClass("btn btn-danger");
+            $("#trueBtn").attr("disabled", true);
+
+        } else {
+            $(this).addClass("btn btn-primary");
+            $("#falseBtn").attr("disabled", true);
+
+        }
+        //console.log("clicked");
+    })
+
+    $(document).on('click', '#completeBtn', function () {
+        stopGame(); //(document) refers to the current page
+
+
+
+    })
+
+    function decrement() {
+
+        //console.log("timer" + gameTimer);
+
+        gameTimer--;
+
+        console.log("timer" + gameTimer);
+        $("#timer").html("<h2>" + gameTimer + "</h2>");
+
+        if (gameTimer === 0) {
+
+            stopGame();
+
+            alert("Time Up!");
+
+
+        }
+    }
+
 })
